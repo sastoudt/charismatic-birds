2e00abbb216e559264d9daa09fcee89f42daba25
Ben Goldstein <ben.goldstein@berkeley.edu>
2021-06-28 11:22:09 -0700

    update vis version

--- 07_vis.R
+++ 07_vis.R
@@ -24,7 +24,7 @@ allData <- read.csv("intermediate_data/GAM_diffs.csv") %>%
   mutate(lb95CI = difference + qnorm(0.025)*diff_SE,
          ub95CI = difference + qnorm(0.975)*diff_SE,
          pval_uncorr = 2*ifelse(pnorm(difference / diff_SE)>0.5, 1-pnorm(difference / diff_SE), pnorm(difference / diff_SE)),
-         )
+  )
 
 # Adjust p-value for false detection rate
 allData$pval <- p.adjust(allData$pval_uncorr,"fdr")
@@ -66,8 +66,8 @@ orderEffects <- read_csv("orderMetaAnalysisResults.csv") %>%
 orderEffects$pval <- p.adjust(orderEffects$pval_uncorr,"fdr")
 
 orderEffects$sigdir <- ifelse(orderEffects$pval < 0.05,
-                         ifelse(orderEffects$beta < 0, "Underreported", "Overreported"),
-                         "Non-significant")
+                              ifelse(orderEffects$beta < 0, "Underreported", "Overreported"),
+                              "Non-significant")
 
 # Interpretation: species are often those that fall into the trait patterns
 # (e.g. peafowl is one of the largest and most beautiful birds, turkeys are
@@ -90,67 +90,67 @@ sigcounts <- allData %>%
 
 # Plot the counts
 (countplot <- allData %>% 
-  mutate(
-    sigdir = factor(sigdir, levels = c(
-      "Underreported", "Non-significant", "Overreported"
-    ))
-  ) %>% 
-  ggplot(aes(sigdir)) +
-  xlab("") + ylab("") +
-  geom_bar(aes(fill = sigdir), show.legend = F) +
-  scale_fill_manual(values = c(negcol, "#888888", poscol)) +
-  geom_text(data = sigcounts, aes(sigdir, n, label = lab), vjust = "bottom", nudge_y = 2) +
-  theme_minimal() +
-  ylim(c(0, 375)) +
-  labs(tag = "A") +
-  theme(axis.ticks = element_blank(),
-        axis.text.y = element_blank(),
-        panel.grid.minor.y = element_blank(),
-        panel.grid.major.x = element_blank()))
+    mutate(
+      sigdir = factor(sigdir, levels = c(
+        "Underreported", "Non-significant", "Overreported"
+      ))
+    ) %>% 
+    ggplot(aes(sigdir)) +
+    xlab("") + ylab("") +
+    geom_bar(aes(fill = sigdir), show.legend = F) +
+    scale_fill_manual(values = c(negcol, "#888888", poscol)) +
+    geom_text(data = sigcounts, aes(sigdir, n, label = lab), vjust = "bottom", nudge_y = 2) +
+    theme_minimal() +
+    ylim(c(0, 375)) +
+    labs(tag = "A") +
+    theme(axis.ticks = element_blank(),
+          axis.text.y = element_blank(),
+          panel.grid.minor.y = element_blank(),
+          panel.grid.major.x = element_blank()))
 
 # Plot the eight most overreported species
 (posplot <- allData %>% 
-  left_join(species_rarity) %>% 
-  mutate(common_name = gsub("_", " ", common_name)) %>% 
-  # filter(rate > 0.01) %>% 
-  filter(abs(difference) < 10) %>% 
-  arrange(-lb95CI) %>%
-  .[1:8,] %>% 
-  arrange(lb95CI) %>% 
-  mutate(common_name = factor(common_name, levels = common_name)) %>%
-  ggplot(aes(common_name, difference)) +
-  geom_errorbar(aes(ymin = lb95CI, ymax = ub95CI)) +
-  geom_point(col = poscol, cex = 3) +
-  coord_flip() +
-  labs(tag = "C") +
-  xlab("") +
-  ylab("") +
-  # ylab("Overreporting index (95% CI)") +
-  theme_minimal() +
-  scale_x_discrete(position = "top") +
-  theme(panel.grid.minor.y = element_blank(),
-        panel.grid.major.x = element_blank()))
+    left_join(species_rarity) %>% 
+    mutate(common_name = gsub("_", " ", common_name)) %>% 
+    # filter(rate > 0.01) %>% 
+    filter(abs(difference) < 10) %>% 
+    arrange(-lb95CI) %>%
+    .[1:8,] %>% 
+    arrange(lb95CI) %>% 
+    mutate(common_name = factor(common_name, levels = common_name)) %>%
+    ggplot(aes(common_name, difference)) +
+    geom_errorbar(aes(ymin = lb95CI, ymax = ub95CI)) +
+    geom_point(col = poscol, cex = 3) +
+    coord_flip() +
+    labs(tag = "C") +
+    xlab("") +
+    ylab("") +
+    # ylab("Overreporting index (95% CI)") +
+    theme_minimal() +
+    scale_x_discrete(position = "top") +
+    theme(panel.grid.minor.y = element_blank(),
+          panel.grid.major.x = element_blank()))
 
 # Plot the eight most underreported species
 (negplot <- allData %>% 
-  left_join(species_rarity) %>% 
-  mutate(common_name = gsub("_", " ", common_name)) %>% 
-  # filter(rate > 0.01) %>% 
-  filter(abs(difference) < 10) %>% 
-  arrange(ub95CI) %>%
-  mutate(common_name = factor(common_name, levels = common_name[length(common_name):1])) %>% 
-  .[1:8,] %>% 
-  ggplot(aes(common_name, difference)) +
-  geom_errorbar(aes(ymin = lb95CI, ymax = ub95CI)) +
-  geom_point(col = negcol, cex = 3) +
-  theme_minimal() +
-  coord_flip() +
-  labs(tag = "B") +
-  xlab("") +
-  ylab("") +
-  # ylab("Overreporting index (95% CI)") +
-  theme(panel.grid.minor.y = element_blank(),
-        panel.grid.major.x = element_blank()))
+    left_join(species_rarity) %>% 
+    mutate(common_name = gsub("_", " ", common_name)) %>% 
+    # filter(rate > 0.01) %>% 
+    filter(abs(difference) < 10) %>% 
+    arrange(ub95CI) %>%
+    mutate(common_name = factor(common_name, levels = common_name[length(common_name):1])) %>% 
+    .[1:8,] %>% 
+    ggplot(aes(common_name, difference)) +
+    geom_errorbar(aes(ymin = lb95CI, ymax = ub95CI)) +
+    geom_point(col = negcol, cex = 3) +
+    theme_minimal() +
+    coord_flip() +
+    labs(tag = "B") +
+    xlab("") +
+    ylab("") +
+    # ylab("Overreporting index (95% CI)") +
+    theme(panel.grid.minor.y = element_blank(),
+          panel.grid.major.x = element_blank()))
 
 Fig2 <- gridExtra::grid.arrange(grobs = list(negplot, countplot, posplot), 
                                 layout_matrix = matrix(c(
@@ -158,7 +158,7 @@ Fig2 <- gridExtra::grid.arrange(grobs = list(negplot, countplot, posplot),
                                   1, 1, 3, 3,
                                   1, 1, 3, 3), byrow = T, nrow= 3
                                 ),
-                        bottom = "Overreporting index (95% CI)")
+                                bottom = "Overreporting index (95% CI)")
 ggsave("output/Fig2.jpg", Fig2, width = 8, height = 4)
 
 
@@ -171,81 +171,106 @@ get_legend<-function(myggplot){
 }
 
 (massplot <- allData %>% 
-  filter(abs(difference) <= 10) %>%
-  mutate(
-    scaled_mass = scale(log(mass)),
-  ) %>% 
-  filter(diff_SE <= 5) %>%
-  ggplot(aes(scaled_mass, difference, col = sigdir, alpha = sigdir)) + 
-  geom_point() +
-  geom_errorbar(aes(ymin = lb95CI, ymax = ub95CI, alpha = sigdir)) +
-  scale_color_manual("", values = c("#888888", poscol, negcol)) +
-  scale_alpha_manual("", values = c(0.3, 1, 1)) +
-  theme_minimal() +
-  labs(tag = "A") +
-  xlab("Scaled log mass") +
-  ylab("Overreporting index"))
+    filter(abs(difference) <= 10) %>%
+    mutate(ub95CI = pmin(ub95CI, 10), lb95CI = pmax(lb95CI, -10)) %>% 
+    # filter(sigdir != "Non-significant") %>% 
+    # left_join(species_rarity) %>% 
+    # left_join(ebird_specs, by = c("species" = "SCIENTIFIC.NAME")) %>%
+    # left_join(trait_dat, by = c("specID"="species_code")) %>% 
+    mutate(
+      scaled_mass = scale(log(mass)),
+    ) %>% 
+    # filter(diff_SE <= 5) %>%
+    ggplot(aes(scaled_mass, difference, col = sigdir, alpha = sigdir)) + 
+    geom_point() +
+    geom_hline(yintercept = 0) +
+    geom_errorbar(aes(ymin = lb95CI, ymax = ub95CI, alpha = sigdir)) +
+    scale_color_manual("", values = c("#888888", poscol, negcol)) +
+    scale_alpha_manual("", values = c(0.15, 1, 1)) +
+    theme_minimal() +
+    theme(panel.grid = element_blank()) +
+    # ylim(c(-10, 10)) +
+    labs(tag = "A") +
+    xlab("Scaled log mass") +
+    ylab("Overreporting index"))
 trait_legend <- get_legend(massplot)
 massplot <- massplot + theme(legend.position = "none", panel.grid.minor = element_blank())
 
 (colorplot <- allData %>% 
-  filter(abs(difference) <= 10) %>% 
-  mutate(
-    scaled_color = scale(max.color.contrast),
-  ) %>% 
-  filter(diff_SE <= 5) %>% 
+    filter(abs(difference) <= 10) %>% 
+    mutate(ub95CI = pmin(ub95CI, 10), lb95CI = pmax(lb95CI, -10)) %>% 
+    # left_join(species_rarity) %>% 
+    # left_join(ebird_specs, by = c("species" = "SCIENTIFIC.NAME")) %>%
+    # left_join(trait_dat, by = c("specID"="species_code")) %>% 
+    mutate(
+      scaled_color = scale(max.color.contrast),
+    ) %>% 
+    # filter(diff_SE <= 5) %>% 
     ggplot(aes(scaled_color, difference, col = sigdir, alpha = sigdir)) + 
     geom_point(show.legend = F) +
+    geom_hline(yintercept = 0) +
     scale_color_manual(values = c("#888888", poscol, negcol)) +
     geom_errorbar(aes(ymin = lb95CI, ymax = ub95CI, alpha = sigdir)) +
-    scale_alpha_manual("", values = c(0.3, 1, 1)) +
-  theme_minimal() +
+    scale_alpha_manual("", values = c(0.15, 1, 1)) +
+    theme_minimal() +
     labs(tag = "B") +
-  xlab("Scaled color contrast") +
+    xlab("Scaled color contrast") +
     ylab("")+
     theme(axis.text.y = element_blank())+
-    theme(legend.position = "none", panel.grid.minor = element_blank())
+    theme(legend.position = "none", panel.grid.minor = element_blank()) +
+    theme(panel.grid = element_blank())
   #ylab("Overreporting index")
-  )
+)
 
 
 (prevplot <- allData %>% 
     filter(abs(difference) <= 10) %>% 
-  left_join(species_rarity) %>% 
-  mutate(
-    scaled_rate = scale(log(rate))
-  ) %>% 
-  filter(diff_SE <= 5) %>% 
+    left_join(species_rarity) %>% 
+    mutate(ub95CI = pmin(ub95CI, 10), lb95CI = pmax(lb95CI, -10)) %>% 
+    # left_join(ebird_specs, by = c("species" = "SCIENTIFIC.NAME")) %>%
+    # left_join(trait_dat, by = c("specID"="species_code")) %>% 
+    mutate(
+      scaled_rate = scale(log(rate))
+    ) %>% 
+    # filter(diff_SE <= 5) %>% 
     ggplot(aes(scaled_rate, difference, col = sigdir, alpha = sigdir)) + 
     geom_point(show.legend = F) +
+    geom_hline(yintercept = 0) +
     scale_color_manual(values = c("#888888", poscol, negcol)) +
     geom_errorbar(aes(ymin = lb95CI, ymax = ub95CI, alpha = sigdir)) +
-    scale_alpha_manual("", values = c(0.3, 1, 1)) +
-  theme_minimal() +
+    scale_alpha_manual("", values = c(0.15, 1, 1)) +
+    theme_minimal() +
     labs(tag = "C") +
-  xlab("Scaled log prevalence") +
-  ylab("Overreporting index")+
-  theme(legend.position = "none", panel.grid.minor = element_blank()))
+    xlab("Scaled log rate") +
+    ylab("Overreporting index")+
+    theme(legend.position = "none", panel.grid.minor = element_blank()) +
+    theme(panel.grid = element_blank()))
 
 (rangeplot <- allData %>% 
     filter(abs(difference) <= 10) %>% 
+    mutate(ub95CI = pmin(ub95CI, 10), lb95CI = pmax(lb95CI, -10)) %>% 
     left_join(species_rarity) %>%
+    # left_join(ebird_specs, by = c("species" = "SCIENTIFIC.NAME")) %>%
+    # left_join(trait_dat, by = c("specID"="species_code")) %>% 
     mutate(
       scaled_nhex = scale(log(nhex))
     ) %>% 
-    filter(diff_SE <= 5) %>% 
+    # filter(diff_SE <= 5) %>% 
     ggplot(aes(scaled_nhex, difference, col = sigdir, alpha = sigdir)) + 
     geom_point(show.legend = F) +
     scale_color_manual(values = c("#888888", poscol, negcol)) +
     geom_errorbar(aes(ymin = lb95CI, ymax = ub95CI, alpha = sigdir)) +
-    scale_alpha_manual("", values = c(0.3, 1, 1)) +
+    scale_alpha_manual("", values = c(0.15, 1, 1)) +
+    geom_hline(yintercept = 0) +
     theme_minimal() +
     labs(tag = "D") +
     xlab("Scaled log range size") +
     ylab("") +
     theme(axis.text.y = element_blank()) +
-    theme(legend.position = "none", panel.grid.minor = element_blank())
-  )
+    theme(legend.position = "none", panel.grid.minor = element_blank()) +
+    theme(panel.grid = element_blank())
+  #    ylab("Overreporting index")
+)
 
 
 Fig3 <- gridExtra::grid.arrange(
@@ -266,20 +291,20 @@ orderEffects_wN <- allData %>%
   mutate(common_order_name_n = paste0(common_order_name, " (n=", n,")") )
 
 (allData %>% 
-  left_join(orderEffects_wN[,c("order", "common_order_name_n")]) %>% 
-  filter(abs(difference) < 10) %>% 
-  ggplot(aes(common_order_name_n, difference)) +
-  geom_hline(yintercept = 0) +
-  geom_boxplot(aes(ymin = lb95CI, ymax = ub95CI), outlier.shape = NA) +
-  geom_pointrange(data = orderEffects_wN, 
-                  aes(common_order_name_n, beta, ymin = lb, ymax = ub, col = sigdir)) +
-  scale_color_manual("", values = c("#888888", poscol, negcol)) +
-  ylab("Overreporting index") +
-  xlab("") +
-  theme_minimal() +
-  coord_flip() +
-  ylim(c(-5.5, 5)) + 
-  theme(panel.grid.major.x = element_blank(), legend.position = "bottom")) %>% 
+    left_join(orderEffects_wN[,c("order", "common_order_name_n")]) %>% 
+    filter(abs(difference) < 10) %>% 
+    ggplot(aes(common_order_name_n, difference)) +
+    geom_hline(yintercept = 0) +
+    geom_boxplot(aes(ymin = lb95CI, ymax = ub95CI), outlier.shape = NA) +
+    geom_pointrange(data = orderEffects_wN, 
+                    aes(common_order_name_n, beta, ymin = lb, ymax = ub, col = sigdir)) +
+    scale_color_manual("", values = c("#888888", poscol, negcol)) +
+    ylab("Overreporting index") +
+    xlab("") +
+    theme_minimal() +
+    coord_flip() +
+    ylim(c(-5.5, 5)) + 
+    theme(panel.grid.major.x = element_blank(), legend.position = "bottom")) %>% 
   ggsave(filename = "output/Fig4.jpg", width = 6, height = 6)
 
 
